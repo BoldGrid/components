7a3e525a858d2cf6807ef90baf4801c0c4abbfeb
Rafael Ramos <rramo012@gmail.com>
2017-12-03 12:25:13 -0500

    Divider progress

--- src/components/divider/config.js
+++ src/components/divider/config.js
@@ -1,5 +1,78 @@
 export default {
-	'styles': [
-		'bg-divider bg-divider-1'
+	styles: [ 'bg-divider bg-divider-1' ],
+	baseName: 'bg-divider',
+	components: [
+		{
+			name: 'clouds',
+			includeTemplate: true
+		},
+		{
+			name: 'curve-async',
+			includeTemplate: true
+		},
+		{
+			name: 'fan-op',
+			includeTemplate: true
+		},
+		{
+			name: 'mountains',
+			includeTemplate: true
+		},
+		{
+			name: 'tilt-op',
+			includeTemplate: true
+		},
+		{
+			name: 'tilt',
+			includeTemplate: true
+		},
+		{
+			name: 'triangle-async',
+			includeTemplate: true
+		},
+		{
+			name: 'waves-smooth',
+			includeTemplate: true
+		},
+		{
+			name: 'pyramid',
+			includeTemplate: true
+		},
+		{
+			name: 'big-triangle',
+			includeTemplate: true
+		},
+		{
+			name: 'curve',
+			includeTemplate: true
+		},
+		{
+			name: 'triangle-shadow',
+			includeTemplate: true
+		},
+		{
+			name: 'drops',
+			includeTemplate: true
+		},
+		{
+			name: 'book',
+			includeTemplate: true
+		},
+		{
+			name: 'split',
+			includeTemplate: true
+		},
+		{
+			name: 'waves',
+			includeTemplate: true
+		},
+		{
+			name: 'arrow',
+			includeTemplate: true
+		},
+		{
+			name: 'half-circle',
+			includeTemplate: true
+		}
 	]
 };
